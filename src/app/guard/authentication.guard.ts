9cdd86eb54710512251c078fc57e01b1b4f39b82
emilykmcullen <emilycullen135@gmail.com>
2021-03-06 16:22:01 +0000

    route guard

---
+++ src/app/guard/authentication.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../service/authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+
+  constructor(private authenticationService: AuthenticationService, private router: Router){
+    
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    return this.isUserLoggedIn();
+  }
+
+  private isUserLoggedIn():  boolean {
+    if(this.authenticationService.isLoggedIn()){
+      return true;
+    }
+    this.router.navigate(['/login']);
+    //Send notification to user
+    return false;
+  }
+  
+}
