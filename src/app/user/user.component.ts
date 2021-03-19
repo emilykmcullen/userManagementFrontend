9d17cb2a4481efb8f4966a30dcec0802700dab8d
emilykmcullen <emilycullen135@gmail.com>
2021-03-19 15:25:59 +0000

    added onSelectUser to user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -16,10 +16,11 @@ export class UserComponent implements OnInit {
   public titleAction$ = this.titleSubject.asObservable();
   public users: User[];
   public refreshing: boolean;
+  public selectedUser: User;
   private subscriptions: Subscription[] = [];
 
   constructor(private userService: UserService, private notificationService: NotificationService) { }
-  
+
   ngOnInit(): void {
     this.getUsers(true);
   }
@@ -46,6 +47,11 @@ export class UserComponent implements OnInit {
       }
     )
 
+  }
+
+  public onSelectUser(selectedUser: User): void {
+    this.selectedUser = selectedUser;
+    document.getElementById('openUserInfo').click();
   }
 
   private sendNotification(notificationType: NotificationType, message: string): void {
